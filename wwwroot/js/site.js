f2ac8b90ea91fb761d4acdee33c06a4134de558f
IGAL SALOMON DUJOVNE <49836363@ort>
2026-06-04 09:02:13 -0300

    4/6

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,23 +2,34 @@
     let letra = document.getElementById("letra").value;
     let palabraOculta = document.getElementById("palabraOculta");
     let palabraCorrecta= document.getElementById("palabraAdivinar").value;
-    let intentos = document.getElementById("intentosRestantes").value;
-    
+    let intentos = parseInt(document.getElementById("intentosRestantes").innerText);
     let nuevaPalabra ="";
+    let encontro = false;
     
     for(let i=0; i<palabraCorrecta.length; i++)
     {
         console.log(letra.toUpperCase() == palabraCorrecta[i]);
         if(letra.toUpperCase() == palabraCorrecta[i])
         {
-            nuevaPalabra += letra ;
+            nuevaPalabra += letra;
+            encontro=true;
         }
         else
         {
-            nuevaPalabra += palabraOculta[i];
+            nuevaPalabra += palabraOculta.innerText[i];            
         }
     }
-    console.log(nuevaPalabra);
+    if (encontro === false)
+    {
+        intentos++;
+    }
+    if(intentos >= 10)
+    {
+        document.getElementById("mensaje").innerText = "Perdiste";    
+    }
     palabraOculta.innerText = nuevaPalabra;
+    document.getElementById("intentosRestantes").innerText = intentos;
+    
+
 
 }
