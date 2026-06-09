4f4c336a5a51bb5a02c5d62efb0b8fecf5d9e76a
IGAL SALOMON DUJOVNE <49836363@ort>
2026-06-09 08:46:17 -0300

    terminado

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,13 +5,21 @@
     let intentos = parseInt(document.getElementById("intentosRestantes").innerText);
     let nuevaPalabra ="";
     let encontro = false;
-    
+    const mensaje = document.getElementById("mensaje");
+    mensaje.innerHTML="";
+    if (ValidarLetra(letra) === false)
+    {
+
+        mensaje.innerHTML = "Debes ingresar una letra"; 
+        return;
+    }
+
     for(let i=0; i<palabraCorrecta.length; i++)
     {
         console.log(letra.toUpperCase() == palabraCorrecta[i]);
         if(letra.toUpperCase() == palabraCorrecta[i])
         {
-            nuevaPalabra += letra;
+            nuevaPalabra += letra.toUpperCase();
             encontro=true;
         }
         else
@@ -19,17 +27,37 @@
             nuevaPalabra += palabraOculta.innerText[i];            
         }
     }
+    
     if (encontro === false)
     {
         intentos++;
     }
     if(intentos >= 10)
     {
-        document.getElementById("mensaje").innerText = "Perdiste";    
+        mensaje.innerText = "Perdiste";  
+        mensaje.style.color= "red";
+        document.getElementById("Arriesgar").disabled = true;
     }
     palabraOculta.innerText = nuevaPalabra;
     document.getElementById("intentosRestantes").innerText = intentos;
     
-
-
+    if (palabraOculta.innerText.includes("_") === false)
+    {
+        mensaje.innerText= "Adivinaste la palabra. Has ganadoooo!"
+        mensaje.style.color= "green";
+        document.getElementById("Arriesgar").disabled = true;
+    }
+    document.getElementById("letra").value = "";
+    document.getElementById("letra").focus();
+}
+function ValidarLetra(letra)
+{
+    if (letra.length === 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
